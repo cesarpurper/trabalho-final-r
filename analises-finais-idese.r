202a44aa8a40f76a1c2ca29d69111b7d7b5bd5af
cesarpurper <48847813+cesarpurper@users.noreply.github.com>
2019-05-31 11:54:14 -0300

    Update analises-finais-idese.r

--- analises-finais-idese.r
+++ analises-finais-idese.r
@@ -8,86 +8,90 @@ library(janitor)
 library(highcharter)
 library(tidyr)
 library(ggplot2)
+library(maptools)
+library(spData)
+library(stringr)
+library(RColorBrewer)
 
 
-idese = readRDS("data/Idese Municipios 2007-2014.rds") %>% 
-  clean_names() 
-
-options(scipen = 999)
-ideseTest = idese %>% 
-  #filter(cod != "4314548") %>% 
+mundo = spData::world %>% 
   mutate(
-    grupoPop = ifelse(populacao<2979, "Pequena", ifelse(populacao<14732, "Média", "Grande"))
+    name_long = as.vector(str_trim(name_long))
+  )
+
+mundo$name_long[mundo$name_long == 'South Sudan'] <- 'Sudan'
+mundo$name_long[mundo$name_long == 'Republic of Korea'] <- 'South Korea'
+mundo$name_long[mundo$name_long == 'Dem. Rep. Korea'] <- 'North Korea'
+
+
+
+felicidade = read.csv("data/felicidade_mundo.csv", encoding = "UTF-8") %>% 
+  clean_names() %>% 
+  mutate(
+    happiness_score = as.double(happiness_score),
+    name = as.character(name)
   )%>% 
-  na.omit() 
+  mutate(
+    name = str_trim(name)
+  ) 
+
+
+
+felicidade$name[felicidade$name == 'Kyrgyz Republic'] = "Kyrgyzstan"
+felicidade$name[felicidade$name == 'Côte d\'Ivoire'] = "Ivory Coast"
+felicidade$name[felicidade$name == 'Russia'] <- 'Russian Federation'
+felicidade$name[felicidade$name == 'Laos'] <- 'Myanmar'
+felicidade$name[felicidade$name == 'Democratic Republic of Congo'] <- 'Democratic Republic of the Congo'
+felicidade$name[felicidade$name == 'Republic of Congo'] <- 'Republic of the Congo'
+
+
+felicidade$name = as.vector(felicidade$name)
+
+
+grouped = felicidade %>% 
+  inner_join(
+    mundo,
+    by = c("name" =  "name_long")
+  )%>% 
+  st_as_sf() 
+
+
+grouped %>% 
+  gather(bloco,valor, economic_liberty, property_rights, government_integrity, judicial_effectiveness, tax_burden, government_spending, fiscal_health, business_freedom, labor_freedom, monetary_freedom, trade_freedom, investment_freedom, financial_freedom) %>% 
+  group_by(continent, index_year, bloco,happiness_score) %>% 
+  summarise(
+    valor = mean(valor)
+  ) %>% 
+  ggplot(aes(x = happiness_score, valor)) +
+  geom_point(aes(color = continent))+
+  geom_smooth(se = FALSE, method = lm)+
+  facet_wrap(~bloco, scales= "free_y")
+
+##mapa mundi com cores no continente
+
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+
+cor_fator <- colorFactor(palette = sample(col_vector, 6), unique(grouped$continent))
+
+tooltip <- sprintf("<strong>%s</strong><br>", 
+                   grouped$name)
+
+leaflet(grouped) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(fillOpacity = 0,
+              weight      = 0.85,
+              color       = "#000000") %>%
   
-  #divisao dos quartis da populacao
-quantile(ideseTest$populacao,probs = seq(0, 1, 0.25), na.rm = FALSE)
-
-
-#comparacao dos blocos por tamanho de cidade
-ideseTest %>% 
-  group_by(grupoPop, ano) %>% 
-  summarise_all(mean)%>% 
-  gather(bloco,valor, bloco_saude,bloco_educacao, bloco_renda, idese) %>% 
-  ggplot(aes(x = ano, valor, color = grupoPop)) +
-  geom_line()+
-  geom_point()+
-  facet_wrap(~bloco, scales = "free_y") 
-
-
-#compração do bloco educacao por tamanho de cidade
-ideseTest %>% 
-  group_by(grupoPop, ano) %>% 
-  summarise_all(mean)%>% 
-  gather(bloco,valor, bloco_educacao, bloco_educacao_ensino_fundamental, bloco_educacao_ensino_fundamental_anos_finais, bloco_educacao_ensino_fundamental_anos_iniciais, bloco_educacao_ensino_medio,bloco_educacao_escolaridade_adulta, bloco_educacao_pre_escola, idese) %>% 
-  ggplot(aes(x = ano, valor, color = grupoPop)) +
-  geom_line()+
-  geom_point()+
-  facet_wrap(~bloco, scales = "free_y") 
-
-#compração do bloco saude por tamanho de cidade
-ideseTest %>% 
-  group_by(grupoPop, ano) %>% 
-  summarise_all(mean)%>% 
-  gather(bloco,valor, bloco_saude,bloco_saude_condicoes_gerais_de_saude, bloco_saude_condicoes_gerais_de_saude_obitos_por_causas_evitaveis, bloco_saude_condicoes_gerais_de_saude_obitos_por_causas_mal_definidas,bloco_saude_longevidade, bloco_saude_saude_materno_infantil, bloco_saude_saude_materno_infantil_consultas_pre_natal,bloco_saude_saude_materno_infantil_mortalidade_de_menores_de_5_anos, idese) %>% 
-  ggplot(aes(x = ano, valor, color = grupoPop)) +
-  geom_line()+
-  geom_point()+
-  facet_wrap(~bloco, scales = "free_y") 
-
-
-
-#compração do bloco renda por tamanho de cidade
-ideseTest %>% 
-  group_by(grupoPop, ano) %>% 
-  summarise_all(mean)%>% 
-  gather(bloco,valor, bloco_renda, bloco_renda_apropriacao_da_renda, bloco_renda_geracao_da_renda, idese) %>% 
-  ggplot(aes(x = ano, valor, color = grupoPop)) +
-  geom_line()+
-  geom_point()+
-  facet_wrap(~bloco, scales = "free_y") 
-
-
-#correlação do bloco educação x idese = 0.8444364
-cor(ideseTest$bloco_educacao, ideseTest$idese)
-
-#correlação do bloco renda x idese = 0.8956826
-cor(ideseTest$bloco_renda, ideseTest$idese)
-
-#correlação do bloco saúde x idese = 0.6263213
-cor(ideseTest$bloco_saude, ideseTest$idese)
-
-
-
-#compração de todos os blocos
-ideseTest %>% 
-  group_by(grupoPop, ano) %>% 
-  summarise_all(mean)%>% 
-  gather(bloco,valor, bloco_educacao, bloco_educacao_ensino_fundamental, bloco_educacao_ensino_fundamental_anos_finais, bloco_educacao_ensino_fundamental_anos_iniciais, bloco_educacao_ensino_medio,bloco_educacao_escolaridade_adulta, bloco_educacao_pre_escola, bloco_saude,bloco_saude_condicoes_gerais_de_saude, bloco_saude_condicoes_gerais_de_saude_obitos_por_causas_evitaveis, bloco_saude_condicoes_gerais_de_saude_obitos_por_causas_mal_definidas,bloco_saude_longevidade, bloco_saude_saude_materno_infantil, bloco_saude_saude_materno_infantil_consultas_pre_natal,bloco_saude_saude_materno_infantil_mortalidade_de_menores_de_5_anos,bloco_renda, bloco_renda_apropriacao_da_renda, bloco_renda_geracao_da_renda, idese) %>% 
-  ggplot(aes(x = ano, valor, color = grupoPop)) +
-  geom_line()+
-  geom_point()+
-  facet_wrap(~bloco, scales = "free_y") 
+  addPolygons(color      = ~cor_fator(continent),
+              stroke      = F,
+              weight      = 0.1,
+              fillOpacity = 0.7,
+              popup       = tooltip) %>%
+  addLegend("bottomright",
+            pal    = cor_fator,
+            values = ~continent,
+            title  = "Continente")
 
 
